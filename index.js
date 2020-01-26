82e67c1a9294eaa849b41e0e27dd46a51bfd7be9
Vedarsh Shah <vedarsh.shah@gmail.com>
2020-01-26 01:31:42 -0500

    Co-authored-by: Aarya Patel <aarya.patel@gmail.com>
    Finished Sorting and Getting data on nearby locations

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
-var user_location;
-
 async function main() {
     var nearby = await getNearbyPlaces();
+    for (var i = 0; i < nearby.results.length; i++) 
+        nearby.results[i].distance = await getDistance(nearby.results[i]);
 
-    console.log(nearby);
+    nearby.results.sort((a, b) => a.distance.value - b.distance.value);
 
-    nearby.results.forEach(element => {
-        console.log(element);
+    for (var i = 0; i < nearby.results.length; i++) 
+        console.log({
+            "name": nearby.results[i].name,
+            "distance":nearby.results[i].distance
+        });
+}
+
+async function getDistance(destination) {
+    var directions = await getDirections(destination);
+
+    return new Promise(resolve => {
+       resolve(directions.routes[0].legs[0].distance); 
+    });
+}
+
+async function getDirections(destination) {
+    var location = await getLocation();
+
+    var gMapsLocation = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+
+    return new Promise(resolve => {
+        
+        var directionsService = new google.maps.DirectionsService();
+
+        var request = {
+            origin: gMapsLocation,
+            destination: {placeId: destination.place_id},
+            travelMode: "WALKING"
+        }
+
+        directionsService.route(request, function(response, status) {
+            if(status == "OK") {
+                resolve(response);
+            }
+        });
     });
 }
 
 async function getNearbyPlaces() {
-    return new Promise((resolve) => {
-
-        function apiNearbyPlaces(geoLocation) {
-            return new Promise((resolve) => {
-                var location = new google.maps.LatLng(geoLocation.coords.latitude, geoLocation.coords.longitude);
-                var request = {
-                    location: location,
-                    radius: '250',
-                };
-                var map = new google.maps.Map(document.getElementById('map'));
-                var service = new google.maps.places.PlacesService(map);
-                service.nearbySearch(request, (results, status) => {
-                    if (status == google.maps.places.PlacesServiceStatus.OK) {
-                        resolve({"results": results, "status" : status});
-                    }
-                });
+    function apiNearbyPlaces(geoLocation) {
+        return new Promise((resolve) => {
+            var location = new google.maps.LatLng(geoLocation.coords.latitude, geoLocation.coords.longitude);
+            var request = {
+                location: location,
+                radius: '250',
+            };
+            var map = new google.maps.Map(document.getElementById('map'));
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, (results, status) => {
+                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    resolve({"results": results, "status" : status});
+                } else {
+                    resolve({"results": [], "status": status});
+                }
             });
-        }
+        });
+    }
 
-        resolve(await apiNearbyPlaces(await getLocation()));
+    var out = await apiNearbyPlaces(await getLocation());
+
+    return new Promise((resolve) =>  {
+        resolve(out);
     });
 }
 
